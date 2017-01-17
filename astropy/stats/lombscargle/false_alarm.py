63564f24d2db2306def8583a86ac260f6209d4b0
Jake VanderPlas <jakevdp@gmail.com>
2017-01-17 07:25:40 -0800

    Add log implementations of quantities

--- astropy/stats/lombscargle/false_alarm.py
+++ astropy/stats/lombscargle/false_alarm.py
@@ -39,6 +39,10 @@ def _gamma(N):
     return np.sqrt(2 / N) * np.exp(gammaln(N / 2) - gammaln((N - 1) / 2))
 
 
+def _log_gamma(N):
+    return 0.5 * np.log(2 / N) + gammaln(N / 2) - gammaln((N - 1) / 2)
+
+
 def vectorize_first_argument(func):
     def vectorized_func(x, *args, **kwargs):
         x = np.asarray(x)
@@ -73,7 +77,7 @@ def inverted(func):
     return invf
 
 
-def P_single(Z, N, normalization='standard', dH=1, dK=3):
+def FAP_single(Z, N, normalization='standard', dH=1, dK=3):
     """
     Cumulative probability for a single frequency
     These are adapted from table 1 of Baluev 2008
@@ -82,18 +86,48 @@ def P_single(Z, N, normalization='standard', dH=1, dK=3):
     NK = N - dK  # DOF for periodic hypothesis
     if normalization == 'psd':
         # 'psd' normalization is same as Baluev's z
-        return 1 - np.exp(-Z)
+        return np.exp(-Z)
     elif normalization == 'standard':
         # 'standard' normalization is Z = 2/NH * z_1
-        return 1 - (1 - Z) ** (NK / 2)
+        return (1 - Z) ** (0.5 * NK)
     elif normalization == 'model':
         # 'model' normalization is Z = 2/NK * z_2
-        return 1 - (1 + Z) ** -(NK / 2)
+        return (1 + Z) ** -(0.5 * NK)
     elif normalization == 'log':
         # 'log' normalization is Z = 2/NK * z_3
-        return 1 - np.exp(-0.5 * NK * Z)
+        return np.exp(-0.5 * NK * Z)
     else:
         raise NotImplementedError("normalization={0}".format(normalization))
+
+
+def log_FAP_single(Z, N, normalization='standard', dH=1, dK=3):
+    """
+    log of cumulative probability for a single frequency
+    These are adapted from table 1 of Baluev 2008
+    """
+    NH = N - dH  # DOF for null hypothesis
+    NK = N - dK  # DOF for periodic hypothesis
+    if normalization == 'psd':
+        # 'psd' normalization is same as Baluev's z
+        return -Z
+    elif normalization == 'standard':
+        # 'standard' normalization is Z = 2/NH * z_1
+        return 0.5 * NK * np.log(1 - Z)
+    elif normalization == 'model':
+        # 'model' normalization is Z = 2/NK * z_2
+        return -0.5 * NK * np.log(1 + Z)
+    elif normalization == 'log':
+        # 'log' normalization is Z = 2/NK * z_3
+        return -0.5 * NK * Z
+    else:
+        raise NotImplementedError("normalization={0}".format(normalization))
+
+
+def P_single(Z, N, normalization='standard', dH=1, dK=3):
+    return 1 - FAP_single(Z, N, normalization=normalization, dH=dH, dK=dK)
+
+def log_P_single(Z, N, normalization='standard', dH=1, dK=3):
+    return np.log1p(-FAP_single(Z, N, normalization=normalization, dH=dH, dK=dK))
 
 
 def tau_davies(Z, fmax, t, y, dy, normalization='standard', dH=1, dK=3):
@@ -123,28 +157,79 @@ def tau_davies(Z, fmax, t, y, dy, normalization='standard', dH=1, dK=3):
         raise NotImplementedError("normalization={0}".format(normalization))
 
 
+def log_tau_davies(Z, fmax, t, y, dy, normalization='standard', dH=1, dK=3):
+    """tau factor for estimating Davies bound (Baluev 2008, Table 1)"""
+    N = len(t)
+    NH = N - dH  # DOF for null hypothesis
+    NK = N - dK  # DOF for periodic hypothesis
+    Dt = _weighted_var(t, dy)
+    Teff = np.sqrt(4 * np.pi * Dt)
+    W = fmax * Teff
+    if normalization == 'psd':
+        # 'psd' normalization is same as Baluev's z
+        return np.log(W) - Z + 0.5 * np.log(Z)
+    elif normalization == 'standard':
+        # 'standard' normalization is Z = 2/NH * z_1
+        return (_log_gamma(NH) + np.log(W) + 0.5 * (NK - 1) * np.log(1 - Z)
+                + 0.5 * np.log(0.5 * NH * Z))
+    elif normalization == 'model':
+        # 'model' normalization is Z = 2/NK * z_2
+        return (_log_gamma(NK) + np.log(W) - 0.5 * NK * np.log(1 + Z)
+                + 0.5 * np.log(0.5 * NK * Z))
+    elif normalization == 'log':
+        # 'log' normalization is Z = 2/NK * z_3
+        return (_log_gamma(NK) + np.log(W) - 0.5 * Z * (NK - 0.5)
+                + 0.5 * np.log(NK * np.sinh(0.5 * Z)))
+    else:
+        raise NotImplementedError("normalization={0}".format(normalization))
+
+
 def FAP_simple(Z, fmax, t, y, dy, normalization='standard'):
     """False Alarm Probability based on estimated number of indep frequencies"""
     N = len(t)
     T = max(t) - min(t)
     N_eff = fmax * T
-    return 1 - P_single(Z, N, normalization=normalization) ** N_eff
+    P_s = P_single(Z, N, normalization=normalization)
+    return 1 - P_s ** N_eff
+
+
+def log_FAP_simple(Z, fmax, t, y, dy, normalization='standard'):
+    """log FAP based on estimated number of indep frequencies"""
+    N = len(t)
+    T = max(t) - min(t)
+    N_eff = fmax * T
+    log_P_s = log_P_single(Z, N, normalization=normalization)
+    return np.log1p(-np.exp(N_eff * log_P_s))
 
 
 def FAP_davies(Z, fmax, t, y, dy, normalization='standard'):
     """Davies bound (Eqn 5 of Baluev 2008)"""
     N = len(t)
-    FAP_s = 1 - P_single(Z, N, normalization=normalization)
+    FAP_s = FAP_single(Z, N, normalization=normalization)
     tau = tau_davies(Z, fmax, t, y, dy, normalization=normalization)
     return FAP_s + tau
 
 
+def log_FAP_davies(Z, fmax, t, y, dy, normalization='standard'):
+    """Davies bound (Eqn 5 of Baluev 2008)"""
+    N = len(t)
+    log_FAP_s = log_FAP_single(Z, N, normalization=normalization)
+    log_tau = log_tau_davies(Z, fmax, t, y, dy, normalization=normalization)
+    return np.logaddexp(log_FAP_s, log_tau)
+
+
 def FAP_baluev(Z, fmax, t, y, dy, normalization='standard'):
     """Alias-free approximation to FAP (Eqn 6 of Baluev 2008)"""
-    N = len(t)
-    P_s = P_single(Z, N, normalization=normalization)
+    P_s = P_single(Z, len(t), normalization=normalization)
     tau = tau_davies(Z, fmax, t, y, dy, normalization=normalization)
     return 1 - P_s * np.exp(-tau)
+
+
+def log_FAP_baluev(Z, fmax, t, y, dy, normalization='standard'):
+    """Alias-free approximation to FAP (Eqn 6 of Baluev 2008)"""
+    log_P_s = log_P_single(Z, len(t), normalization=normalization)
+    tau = tau_davies(Z, fmax, t, y, dy, normalization=normalization)
+    return np.log1p(-np.exp(log_P_s - tau))
 
 
 def _bootstrap(fmax, t, y, dy, normalization='standard',
--- astropy/stats/lombscargle/tests/test_false_alarm.py
+++ astropy/stats/lombscargle/tests/test_false_alarm.py
@@ -1,8 +1,10 @@
 import numpy as np
+from numpy.testing import assert_allclose
 
 from ....tests.helper import pytest
 
-from ..false_alarm import false_alarm_probability, significance_level, METHODS
+from .. import false_alarm
+from ..false_alarm import false_alarm_probability, significance_level, METHODS, inverted
 from .. import LombScargle
 
 
@@ -21,6 +23,31 @@ def data(N=100, period=1, theta=[10, 2, 3], dy=1, rseed=0):
     y += dy * rng.randn(N)
 
     return t, y, dy
+
+
+# @pytest.mark.parametrize('method', set(METHODS) - {'bootstrap'})
+# @pytest.mark.parametrize('normalization', NORMALIZATIONS)
+# @pytest.mark.parametrize('fmax', [5])
+# def test_inverted_function(method, normalization, fmax, data):
+#     kwds = METHOD_KWDS.get(method, None)
+#
+#     # shuffle the data to find useful range of Z
+#     t, y, dy = data
+#     rng = np.random.RandomState(0)
+#     i = np.arange(len(t))
+#     rng.shuffle(i)
+#     y, dy = y[i], dy[i]
+#     freq, power = LombScargle(t, y, dy).autopower(normalization=normalization,
+#                                                   maximum_frequency=fmax)
+#     Z = np.linspace(power.min(), power.max(), 30)
+#
+#     args = (fmax, t, y, dy)
+#     kwargs = dict(normalization=normalization,
+#                   method=method,
+#                   method_kwds=METHOD_KWDS.get(method, None))
+#     fap = false_alarm_probability(Z, *args, **kwargs)
+#     Z_out = inverted(false_alarm_probability)(fap, *args, **kwargs)
+#     assert_allclose(Z, Z_out, rtol=1E-8, atol=1E-8)
 
 
 @pytest.mark.parametrize('method', METHODS)
@@ -50,9 +77,8 @@ def test_false_alarm_smoketest(method, normalization, fmax, data):
 def test_significance_smoketest(method, normalization, fmax, data):
     kwds = METHOD_KWDS.get(method, None)
     t, y, dy = data
-    fmax = 5
 
-    # sig can't go above 0.9 because bootstrap limited to 10 samples
+    # sig can't go above 0.95 because bootstrap limited to 20 samples
     sig = np.array([0.5, 0.6, 0.7, 0.8, 0.9, 0.95])
     Z = significance_level(sig, fmax, t, y, dy,
                            normalization=normalization,
@@ -63,6 +89,115 @@ def test_significance_smoketest(method, normalization, fmax, data):
     assert np.all(Z[:-1] <= Z[1:])  # monotonically increasing
 
 
-# TODO: test inverted() round-trip on a couple functions
+@pytest.mark.parametrize('method', METHODS)
+@pytest.mark.parametrize('normalization', NORMALIZATIONS)
+@pytest.mark.parametrize('fmax', [5])
+def test_log_fap_single(method, normalization, fmax, data):
+    kwds = METHOD_KWDS.get(method, None)
+    t, y, dy = data
 
-# TODO: test that significance & fap are consistent with each other
+    freq, power = LombScargle(t, y, dy).autopower(normalization=normalization)
+    Z = np.linspace(power.min(), power.max(), 30)
+
+    FAP = false_alarm.FAP_single(Z, len(t), normalization)
+    logFAP = false_alarm.log_FAP_single(Z, len(t), normalization)
+
+    assert_allclose(FAP, np.exp(logFAP))
+
+
+@pytest.mark.parametrize('method', METHODS)
+@pytest.mark.parametrize('normalization', NORMALIZATIONS)
+@pytest.mark.parametrize('fmax', [5])
+def test_log_P_single(method, normalization, fmax, data):
+    kwds = METHOD_KWDS.get(method, None)
+    t, y, dy = data
+
+    freq, power = LombScargle(t, y, dy).autopower(normalization=normalization)
+    Z = np.linspace(power.min(), power.max(), 30)
+
+    P = false_alarm.P_single(Z, len(t), normalization)
+    logP = false_alarm.log_P_single(Z, len(t), normalization)
+
+    assert_allclose(P, np.exp(logP))
+
+
+@pytest.mark.parametrize('method', METHODS)
+@pytest.mark.parametrize('normalization', NORMALIZATIONS)
+@pytest.mark.parametrize('fmax', [5])
+def test_log_tau_davies(method, normalization, fmax, data):
+    kwds = METHOD_KWDS.get(method, None)
+    t, y, dy = data
+
+    freq, power = LombScargle(t, y, dy).autopower(normalization=normalization)
+    Z = np.linspace(power.min(), power.max(), 30)
+
+    tau = false_alarm.tau_davies(Z, fmax, t, y, dy, normalization)
+    log_tau = false_alarm.log_tau_davies(Z, fmax, t, y, dy, normalization)
+
+    assert_allclose(tau, np.exp(log_tau))
+
+
+@pytest.mark.parametrize('method', METHODS)
+@pytest.mark.parametrize('normalization', NORMALIZATIONS)
+@pytest.mark.parametrize('fmax', [5])
+def test_log_FAP_simple(method, normalization, fmax, data):
+    kwds = METHOD_KWDS.get(method, None)
+    t, y, dy = data
+
+    freq, power = LombScargle(t, y, dy).autopower(normalization=normalization)
+    Z = np.linspace(power.min(), power.max(), 30)
+
+    FAP = false_alarm.FAP_simple(Z, fmax, t, y, dy, normalization)
+    logFAP = false_alarm.log_FAP_simple(Z, fmax, t, y, dy, normalization)
+
+    assert_allclose(FAP, np.exp(logFAP), atol=1E-14)
+
+
+@pytest.mark.parametrize('method', METHODS)
+@pytest.mark.parametrize('normalization', NORMALIZATIONS)
+@pytest.mark.parametrize('fmax', [5])
+def test_log_FAP_davies(method, normalization, fmax, data):
+    kwds = METHOD_KWDS.get(method, None)
+    t, y, dy = data
+
+    freq, power = LombScargle(t, y, dy).autopower(normalization=normalization)
+    Z = np.linspace(power.min(), power.max(), 30)
+
+    FAP = false_alarm.FAP_davies(Z, fmax, t, y, dy, normalization)
+    logFAP = false_alarm.log_FAP_davies(Z, fmax, t, y, dy, normalization)
+
+    assert_allclose(FAP, np.exp(logFAP), atol=1E-14)
+
+
+@pytest.mark.parametrize('method', METHODS)
+@pytest.mark.parametrize('normalization', NORMALIZATIONS)
+@pytest.mark.parametrize('fmax', [5])
+def test_log_FAP_baluev(method, normalization, fmax, data):
+    kwds = METHOD_KWDS.get(method, None)
+    t, y, dy = data
+
+    freq, power = LombScargle(t, y, dy).autopower(normalization=normalization)
+    Z = np.linspace(power.min(), power.max(), 30)
+
+    FAP = false_alarm.FAP_baluev(Z, fmax, t, y, dy, normalization)
+    logFAP = false_alarm.log_FAP_baluev(Z, fmax, t, y, dy, normalization)
+
+    assert_allclose(FAP, np.exp(logFAP), atol=1E-14)
+
+
+# @pytest.mark.parametrize('method', METHODS)
+# @pytest.mark.parametrize('normalization', NORMALIZATIONS)
+# @pytest.mark.parametrize('fmax', [5])
+# def test_false_alarm_roundtrip(method, normalization, fmax, data):
+#     # sig can't go above 0.95 because bootstrap limited to 20 samples
+#     t, y, dy = data
+#     sig = np.array([0.5, 0.6, 0.7, 0.8, 0.9, 0.95])
+#     Z = significance_level(sig, fmax, t, y, dy,
+#                            normalization=normalization,
+#                            method=method,
+#                            method_kwds=METHOD_KWDS.get(method, None))
+#     fap = false_alarm_probability(Z, fmax, t, y, dy,
+#                                   normalization=normalization,
+#                                   method=method,
+#                                   method_kwds=METHOD_KWDS.get(method, None))
+#     assert_allclose(1 - sig, fap)
